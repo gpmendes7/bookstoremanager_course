88ad0ffb41c07212bdca6a30d484fac636ffa505
gpmendes7 <andrewsseven@gmail.com>
2023-05-12 11:56:12 -0300

    developed author creation functionality

--- src/main/java/com/rodrigopeleias/bookstoremanager/author/service/AuthorService.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/author/service/AuthorService.java
@@ -1,5 +1,8 @@
 package com.rodrigopeleias.bookstoremanager.author.service;
 
+import com.rodrigopeleias.bookstoremanager.author.dto.AuthorDTO;
+import com.rodrigopeleias.bookstoremanager.author.entity.Author;
+import com.rodrigopeleias.bookstoremanager.author.exception.AuthorAlreadyExistsException;
 import com.rodrigopeleias.bookstoremanager.author.mapper.AuthorMapper;
 import com.rodrigopeleias.bookstoremanager.author.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +20,17 @@ public class AuthorService {
         this.authorRepository = authorRepository;
     }
 
+    public AuthorDTO create(AuthorDTO authorDTO) {
+        verifyIfExists(authorDTO.getName());
+
+        Author authorToCreate = authorMapper.toModel(authorDTO);
+        Author createdAuthor = authorRepository.save(authorToCreate);
+        return authorMapper.toDTO(createdAuthor);
+    }
+
+    private void verifyIfExists(String authorName) {
+        authorRepository.findByName(authorName)
+                .ifPresent(author -> {throw new AuthorAlreadyExistsException(authorName);});
+    }
 
 }
