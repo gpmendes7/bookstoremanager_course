82c70d7be208a60428ffad55a5cd6ad2cb35d251
gpmendes7 <andrewsseven@gmail.com>
2023-05-23 16:06:51 -0300

    developed user creation feature

--- src/test/java/com/rodrigopeleias/bookstoremanager/users/controller/UserControllerTest.java
+++ src/test/java/com/rodrigopeleias/bookstoremanager/users/controller/UserControllerTest.java
@@ -1,20 +1,30 @@
 package com.rodrigopeleias.bookstoremanager.users.controller;
 
 import com.rodrigopeleias.bookstoremanager.users.builder.UserDTOBuilder;
+import com.rodrigopeleias.bookstoremanager.users.dto.MessageDTO;
+import com.rodrigopeleias.bookstoremanager.users.dto.UserDTO;
 import com.rodrigopeleias.bookstoremanager.users.service.UserService;
+import com.rodrigopeleias.bookstoremanager.utils.JsonConversionUtils;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
 
+    private static final String USERS_API_URL_PATH = "/api/v1/users";
     private MockMvc mockMvc;
 
     @Mock
@@ -32,5 +42,33 @@ public class UserControllerTest {
                 .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
                 .build();
+    }
+
+    @Test
+    void whenPOSTIsCalledThenCreatedStatusShouldBeReturned() throws Exception {
+        UserDTO expectedUserToCreateDTO = userDTOBuilder.buildUserDTO();
+        String expectedCreationMessage = "User rodrigopeleias with ID 1 successfully created";
+        MessageDTO expectedCreationMessageDTO = MessageDTO.builder().message(expectedCreationMessage).build();
+
+        Mockito.when(userService.create(expectedUserToCreateDTO)).thenReturn(expectedCreationMessageDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(USERS_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonConversionUtils.asJsonString(expectedUserToCreateDTO)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is(expectedCreationMessage)));
+
+    }
+
+    @Test
+    void whenPOSTIsCalledWithoutRequiredFieldThenBadRequestStatusShouldBeReturned() throws Exception {
+        UserDTO expectedUserToCreateDTO = userDTOBuilder.buildUserDTO();
+        expectedUserToCreateDTO.setUsername(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(USERS_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonConversionUtils.asJsonString(expectedUserToCreateDTO)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
     }
 }
